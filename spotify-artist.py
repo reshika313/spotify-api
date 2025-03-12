630c0230894e3d7b7451807c74d06f140f3384eb
Reshika Chilakapati <107808909+reshika313@users.noreply.github.com>
2025-03-12 16:24:42 +0000

    Update spotify-artist.py

--- spotify-artist.py
+++ spotify-artist.py
@@ -1,21 +1,22 @@
+#1. importing libraries needed
 import requests
 
-# The Access Token generated through your OAuth process
+#2. the acccess token generated through your OAuth process
 ACCESS_TOKEN = " "
 
-# Spotify Artist ID for Travis Scott
+#3. spotify artist ID for Travis Scott
 artist_id = "0Y5tJX1MQlPlqiwlOH1tJY" 
 
-# Spotify API URL for the artist
+#4. spotify api URL for the artist
 artist_url = f"https://api.spotify.com/v1/artists/" + artist_id
 
-# Headers for the request, including the Authorization token
+#5. headers for the request, including the Authorization token
 headers = {
     "Authorization": "Bearer " + ACCESS_TOKEN
 }
 
-# Make the GET request
+#6. making the GET request
 response = requests.get(artist_url, headers=headers)
 
-# Print the response (artist details)
+#7. printing the response (artist details)
 print(response.json())
